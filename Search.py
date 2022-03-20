69dfd4129038346efaffc77e003b2a06491e39c9
unknown <minsuft19@gmail.com>
2022-03-20 20:14:16 +0900

    release 1.0

--- Search.py
+++ Search.py
@@ -1,3 +1,4 @@
+from pickle import FALSE
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
@@ -6,27 +7,95 @@ import json
 import selenium
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver import Firefox
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
 
+import subprocess
 import pyautogui
+import socks
+from stem import Signal
+from stem.control import Controller
+import os
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-#
+import tbselenium.common as cm
+from tbselenium.tbdriver import TorBrowserDriver
+from tbselenium.utils import launch_tbb_tor_with_stem
+from random_user_agent.user_agent import UserAgent
+from random_user_agent.params import SoftwareName, OperatingSystem
+
+import random
+
+
+
+
 #    Get Response.
 #
 def getRes(url, headers):
     res = requests.get(url, headers = headers)
-    time.sleep(1)
+    time.sleep(3)
     res.raise_for_status()
     soup = BeautifulSoup(res.text, "lxml")
         
     return res
+
+def keyDownEnd():
+    time.sleep(1)
+    pyautogui.press('end')
+    time.sleep(1)
+    pyautogui.press('end')
+    time.sleep(1)
+    pyautogui.press('end')
+    time.sleep(1)
+    pyautogui.press('end')
+    time.sleep(1)
+    pyautogui.press('end')
+    time.sleep(1)
+
+def keyDownPageDown():
+    time.sleep(1)
+    pyautogui.press('pagedown')
+    time.sleep(1)
+    pyautogui.press('pagedown')
+    time.sleep(1)
+    pyautogui.press('pagedown')
+    time.sleep(1)
+    pyautogui.press('pagedown')
+    time.sleep(1)
+    pyautogui.press('pagedown')
+    time.sleep(1)
+
+def keyDownPageUp():
+    time.sleep(1)
+    pyautogui.press('pageup')
+    time.sleep(1)
+    pyautogui.press('pageup')
+    time.sleep(1)
+    pyautogui.press('pageup')
+    time.sleep(1)
+    pyautogui.press('pageup')
+    time.sleep(1)
+    pyautogui.press('pageup')
+    time.sleep(1)    
 
 
 def SearchKeyword(keyword, LinkText):
 
     options = webdriver.ChromeOptions()
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    
+    software_names = [SoftwareName.CHROME.value]
+    operating_systems = [OperatingSystem.WINDOWS.value, OperatingSystem.LINUX.value]
+    user_agent_rotator = UserAgent(software_names=software_names, operating_systems=operating_systems, limit=100)
+
+    userAgent = user_agent_rotator.get_random_user_agent()
+
+    options.add_argument(f'user-agent={userAgent}')
     options.w3c = True
     driver = webdriver.Chrome(options=options)
 
@@ -44,25 +113,13 @@ def SearchKeyword(keyword, LinkText):
     tmp = element.click()
     time.sleep(1)
 
-    print(f"handles : {driver.window_handles}")
     driver.switch_to.window(driver.window_handles[1])
 
 
     for cnt in range(1, 10):
         # 화면 내리기 end 키로.
-        time.sleep(1)
-        pyautogui.press('end')
-        time.sleep(1)
-        pyautogui.press('end')
-        time.sleep(1)
-        pyautogui.press('end')
-        time.sleep(1)
-        pyautogui.press('end')
-        time.sleep(1)
-        pyautogui.press('end')
-        time.sleep(1)
+        keyDownEnd()
 
-        # 나성유통 타포린백
         try:
             naseong = driver.find_element(By.LINK_TEXT, LinkText)
             naseong.click()
@@ -73,7 +130,9 @@ def SearchKeyword(keyword, LinkText):
             time.sleep(1)
             continue
 
-        time.sleep(20)
+        keyDownPageDown()
+
+        time.sleep(60)
 
         # Close chrome.
         driver.quit()
@@ -87,13 +146,16 @@ def getJsonInfo(filePath):
         return array
 
 if __name__ == "__main__":
-    
+
+    cnt = 0
     for val in range(0, 70):
+        cnt += 1
         filePath = './searchInfo.json'  # json 파일.
         Array = getJsonInfo(filePath)
-        time.sleep(1)
+        print(f"count : {cnt}")
+        time.sleep(2)
 
         for keywordInfo in Array:
+            #print(f"{keywordInfo['키워드']}, {keywordInfo['제목']}")
             SearchKeyword(keywordInfo['키워드'], keywordInfo['제목'])
-        time.sleep(60)
-
+        time.sleep(random.randrange(60, 80))
